8f57ad004b4ffe1b57a72e52f363416a1b73eb24
Jimmy Lee <jimmyminhlee@gmail.com>
2022-02-07 16:42:27 -0800

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,40 +1,17 @@
 import React from 'react';
-import {
-  ChakraProvider,
-  Box,
-  Text,
-  Link,
-  VStack,
-  Code,
-  Grid,
-  theme,
-} from '@chakra-ui/react';
-import { ColorModeSwitcher } from './ColorModeSwitcher';
-import { Logo } from './Logo';
+import { ChakraProvider } from '@chakra-ui/react';
+import NavigationBar from './Components/NavigationBar.js';
+import Index from './Pages/Index.js';
+import { BrowserRouter } from 'react-router-dom';
+import theme from './Meta/theme';
 
 function App() {
   return (
     <ChakraProvider theme={theme}>
-      <Box textAlign="center" fontSize="xl">
-        <Grid minH="100vh" p={3}>
-          <ColorModeSwitcher justifySelf="flex-end" />
-          <VStack spacing={8}>
-            <Logo h="40vmin" pointerEvents="none" />
-            <Text>
-              Edit <Code fontSize="xl">src/App.js</Code> and save to reload.
-            </Text>
-            <Link
-              color="teal.500"
-              href="https://chakra-ui.com"
-              fontSize="2xl"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn Chakra
-            </Link>
-          </VStack>
-        </Grid>
-      </Box>
+      <BrowserRouter>
+        <NavigationBar />
+        <Index />
+      </BrowserRouter>
     </ChakraProvider>
   );
 }
